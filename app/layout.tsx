3e014e6b86da5e7ff33c3647da1170b7d9035cdf
Chris Panteli <chrispanteli@MacBook-Air-9.local>
2026-03-13 18:00:34 +0200

    Add Meta Pixel global tracking with provided ID

--- app/layout.tsx
+++ app/layout.tsx
@@ -107,6 +107,26 @@ export default function RootLayout({
             gtag('config', 'G-V5RGLHQ7W5');
           `}
         </Script>
+        <Script id="meta-pixel-init" strategy="afterInteractive">
+          {`
+            !function(f,b,e,v,n,t,s){
+              if(f.fbq)return;
+              n=f.fbq=function(){n.callMethod ? n.callMethod.apply(n,arguments) : n.queue.push(arguments)};
+              if(!f._fbq)f._fbq=n;
+              n.push=n;
+              n.loaded=!0;
+              n.version='2.0';
+              n.queue=[];
+              t=b.createElement(e);
+              t.async=!0;
+              t.src=v;
+              s=b.getElementsByTagName(e)[0];
+              s.parentNode.insertBefore(t,s);
+            }(window, document,'script','https://connect.facebook.net/en_US/fbevents.js');
+            fbq('init', '1173118473123111');
+            fbq('track', 'PageView');
+          `}
+        </Script>
         <Script
           id="clearbit-tags"
           src="https://tag.clearbitscripts.com/v1/pk_f2aefa5c8fd1bd2d23a69b6c4b47fe6a/tags.js"
